54881e8459471c47af14973dc95a311b3bfad437
IJBP97 <112659908+IJBP97@users.noreply.github.com>
2025-09-02 13:56:44 -0600

    Add files via upload

---
+++ hotel.sql
@@ -0,0 +1,493 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-09-2025 a las 21:56:09
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hotel`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_PrecioTotalHabitacion` (IN `p_id_habitacion` INT)   BEGIN
+    SELECT h.id_habitacion,
+           h.numero_habitacion,
+           h.precio AS PrecioBase,
+           IFNULL(SUM(s.costo),0) AS TotalServicios,
+           h.precio + IFNULL(SUM(s.costo),0) AS PrecioTotal
+    FROM Habitacion h
+    LEFT JOIN Habitacion_Servicio hs ON h.id_habitacion = hs.id_habitacion
+    LEFT JOIN Servicio s ON hs.id_servicio = s.id_servicio
+    WHERE h.id_habitacion = p_id_habitacion
+    GROUP BY h.id_habitacion, h.numero_habitacion, h.precio;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `area`
+--
+
+CREATE TABLE `area` (
+  `id_area` int(11) NOT NULL,
+  `nombre_area` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `area`
+--
+
+INSERT INTO `area` (`id_area`, `nombre_area`) VALUES
+(1, 'Recepción'),
+(2, 'Limpieza'),
+(3, 'Restaurante');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `checkinout`
+--
+
+CREATE TABLE `checkinout` (
+  `id_check` int(11) NOT NULL,
+  `hora_entrada` datetime NOT NULL,
+  `hora_salida` datetime DEFAULT NULL,
+  `id_reservacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido_paterno` varchar(50) NOT NULL,
+  `apellido_materno` varchar(50) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellido_paterno`, `apellido_materno`, `correo`, `telefono`) VALUES
+(1, 'Juan', 'Pérez', 'López', 'juan.perez@example.com', '555-1234'),
+(2, 'María', 'García', 'Hernández', 'maria.garcia@example.com', '555-5678'),
+(3, 'Carlos', 'Ramírez', 'Ramirez', 'carlos.ramirez@example.com', '555-8765');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cobro`
+--
+
+CREATE TABLE `cobro` (
+  `id_cobro` int(11) NOT NULL,
+  `fecha_transaccion` datetime NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
+  `id_ticket` int(11) NOT NULL,
+  `id_tipo_pago` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido_paterno` varchar(50) NOT NULL,
+  `apellido_materno` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `area` int(11) NOT NULL,
+  `turno` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido_paterno`, `apellido_materno`, `telefono`, `area`, `turno`) VALUES
+(1, 'Laura', 'Martínez', 'Gomez', '555-1111', 1, 1),
+(2, 'Pedro', 'Sánchez', 'Gómez', '555-2222', 2, 2),
+(3, 'Ana', 'Torres', 'Flores', '555-3333', 3, 3),
+(4, 'ggrgr', 'grgrg', 'grgrg', 'g4444', 1, 2),
+(5, 'ggrgr', 'grgrg', 'grgrg', 'g4444', 1, 2),
+(6, 'fff', 'ff', 'ff', '556464646', 1, 1),
+(7, 'fff', 'ff', 'ff', '556464646', 1, 1),
+(8, 'prueba', 'prueba', '', '', 1, 1),
+(9, 'hola', 'hola', 'hla', 'fee', 1, 1),
+(10, 'torres10', 'Ramírez', 'Ramirez', '555-8765', 1, 3),
+(11, 'oro', 'Ramírez', 'Ramirez', '555-8765', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `id_habitacion` int(11) NOT NULL,
+  `numero_habitacion` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `tipo_habitacion` varchar(50) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `cantidad_personas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `habitacion`
+--
+
+INSERT INTO `habitacion` (`id_habitacion`, `numero_habitacion`, `precio`, `tipo_habitacion`, `estado`, `cantidad_personas`) VALUES
+(1, 101, 1200.00, 'Sencilla', 'Disponible', 2),
+(2, 102, 1800.00, 'Doble', 'Ocupada', 4),
+(3, 103, 2500.00, 'Suite', 'Mantenimiento', 5),
+(5, 1, 1500.00, 'Doble', 'Disponible', 2),
+(6, 106, 1800.00, 'Suite', 'Disponible', 25),
+(7, 110, 1600.00, 'Sencilla', 'Disponible', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion_servicio`
+--
+
+CREATE TABLE `habitacion_servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `id_habitacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Disparadores `habitacion_servicio`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_UpdatePrecioHabitacion` AFTER INSERT ON `habitacion_servicio` FOR EACH ROW BEGIN
+    UPDATE Habitacion h
+    JOIN Servicio s ON NEW.id_servicio = s.id_servicio
+    SET h.precio = h.precio + s.costo
+    WHERE h.id_habitacion = NEW.id_habitacion;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservacion`
+--
+
+CREATE TABLE `reservacion` (
+  `id_reservacion` int(11) NOT NULL,
+  `fecha_reservacion` date NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_habitacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `nombre_servicio` varchar(100) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `costo` decimal(10,2) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_reservacion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id_ticket` int(11) NOT NULL,
+  `numero_ticket` varchar(50) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_reservacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipopago`
+--
+
+CREATE TABLE `tipopago` (
+  `id_tipo_pago` int(11) NOT NULL,
+  `metodo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipopago`
+--
+
+INSERT INTO `tipopago` (`id_tipo_pago`, `metodo`) VALUES
+(1, 'Efectivo'),
+(2, 'Tarjeta Crédito'),
+(3, 'Transferencia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno`
+--
+
+CREATE TABLE `turno` (
+  `id_turno` int(11) NOT NULL,
+  `tipo_turno` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `turno`
+--
+
+INSERT INTO `turno` (`id_turno`, `tipo_turno`) VALUES
+(1, 'AM'),
+(2, 'PM'),
+(3, 'NOC');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`id_area`);
+
+--
+-- Indices de la tabla `checkinout`
+--
+ALTER TABLE `checkinout`
+  ADD PRIMARY KEY (`id_check`),
+  ADD KEY `id_reservacion` (`id_reservacion`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- Indices de la tabla `cobro`
+--
+ALTER TABLE `cobro`
+  ADD PRIMARY KEY (`id_cobro`),
+  ADD KEY `id_ticket` (`id_ticket`),
+  ADD KEY `id_tipo_pago` (`id_tipo_pago`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD KEY `area` (`area`),
+  ADD KEY `turno` (`turno`);
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`id_habitacion`),
+  ADD UNIQUE KEY `numero_habitacion` (`numero_habitacion`);
+
+--
+-- Indices de la tabla `habitacion_servicio`
+--
+ALTER TABLE `habitacion_servicio`
+  ADD PRIMARY KEY (`id_servicio`,`id_habitacion`),
+  ADD KEY `id_habitacion` (`id_habitacion`);
+
+--
+-- Indices de la tabla `reservacion`
+--
+ALTER TABLE `reservacion`
+  ADD PRIMARY KEY (`id_reservacion`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_habitacion` (`id_habitacion`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_reservacion` (`id_reservacion`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id_ticket`),
+  ADD UNIQUE KEY `numero_ticket` (`numero_ticket`),
+  ADD KEY `id_reservacion` (`id_reservacion`);
+
+--
+-- Indices de la tabla `tipopago`
+--
+ALTER TABLE `tipopago`
+  ADD PRIMARY KEY (`id_tipo_pago`);
+
+--
+-- Indices de la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id_turno`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `area`
+--
+ALTER TABLE `area`
+  MODIFY `id_area` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `checkinout`
+--
+ALTER TABLE `checkinout`
+  MODIFY `id_check` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `cobro`
+--
+ALTER TABLE `cobro`
+  MODIFY `id_cobro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  MODIFY `id_habitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `reservacion`
+--
+ALTER TABLE `reservacion`
+  MODIFY `id_reservacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tipopago`
+--
+ALTER TABLE `tipopago`
+  MODIFY `id_tipo_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `checkinout`
+--
+ALTER TABLE `checkinout`
+  ADD CONSTRAINT `checkinout_ibfk_1` FOREIGN KEY (`id_reservacion`) REFERENCES `reservacion` (`id_reservacion`);
+
+--
+-- Filtros para la tabla `cobro`
+--
+ALTER TABLE `cobro`
+  ADD CONSTRAINT `cobro_ibfk_1` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id_ticket`),
+  ADD CONSTRAINT `cobro_ibfk_2` FOREIGN KEY (`id_tipo_pago`) REFERENCES `tipopago` (`id_tipo_pago`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`area`) REFERENCES `area` (`id_area`),
+  ADD CONSTRAINT `empleado_ibfk_2` FOREIGN KEY (`turno`) REFERENCES `turno` (`id_turno`);
+
+--
+-- Filtros para la tabla `habitacion_servicio`
+--
+ALTER TABLE `habitacion_servicio`
+  ADD CONSTRAINT `habitacion_servicio_ibfk_1` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`),
+  ADD CONSTRAINT `habitacion_servicio_ibfk_2` FOREIGN KEY (`id_habitacion`) REFERENCES `habitacion` (`id_habitacion`);
+
+--
+-- Filtros para la tabla `reservacion`
+--
+ALTER TABLE `reservacion`
+  ADD CONSTRAINT `reservacion_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `reservacion_ibfk_2` FOREIGN KEY (`id_habitacion`) REFERENCES `habitacion` (`id_habitacion`);
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`),
+  ADD CONSTRAINT `servicio_ibfk_2` FOREIGN KEY (`id_reservacion`) REFERENCES `reservacion` (`id_reservacion`);
+
+--
+-- Filtros para la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_reservacion`) REFERENCES `reservacion` (`id_reservacion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
